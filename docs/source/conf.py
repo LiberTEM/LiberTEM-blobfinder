d6078b43e23e06fee333352ccba8e4edb3206aa1
Dieter Weber <d.weber@fz-juelich.de>
2020-02-11 17:41:55 +0100

    Review @sk1p, small fixes
    Thx! :-)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -207,9 +207,9 @@ doctest_test_doctest_blocks = ''
 todo_include_todos = True
 
 intersphinx_mapping = {
-    'numpy': ('http://docs.scipy.org/doc/numpy/', None),
+    'numpy': ('https://docs.scipy.org/doc/numpy/', None),
     'python': ('https://docs.python.org/3.7', None),
-    'libertem': ('http://libertem.github.io/LiberTEM/', None),
+    'libertem': ('https://libertem.github.io/LiberTEM/', None),
 }
 
 # Sphinx' link checker.
